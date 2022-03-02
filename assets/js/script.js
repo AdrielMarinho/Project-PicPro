686a184d64fa1ae78fe1d88829bec64267621b28
AdrielMarinho <adrielqmarinho@hotmail.com>
2022-03-02 19:48:35 -0300

    Projeto Semi Finalizado
    Próximo pedaço é ajustar a parte dos desafios.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,24 @@ const listaOpcoes = document.querySelector(".secaoDetalhesConta_opcoesTransacoes
 listaOpcoes.addEventListener("click", identificarOpcoes)
 
 function identificarOpcoes(event) {
-    const li = event.target 
+    const elemento = event.target 
     
-    if (li.tagName == "LI") {
-        console.log(li.id)
+    if (elemento.tagName == "LI") {
+        const id = elemento.id
+
+        const secaoEscolha = document.querySelector(`div[data-id="${id}"]`)
+        
+        removeClass()
+
+        secaoEscolha.classList.add("mostrar")
+
+    }
+}
+
+function removeClass(){
+    const divs = document.querySelectorAll(".secaoTransacao .container div")
+
+    for (let i = 0; i < divs.length; i++) {
+        divs[i].classList.remove("mostrar")
     }
 }
